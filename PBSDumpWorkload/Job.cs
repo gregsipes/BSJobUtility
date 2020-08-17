48947bdc832144f2f5b299071f9c8103fa13f3fd
Greg Sipes <gregory.sipes@gmail.com>
2020-08-17 14:53:25 -0400

    pbsdumpworkload changes

--- PBSDumpWorkload/Job.cs
+++ PBSDumpWorkload/Job.cs
@@ -124,13 +124,17 @@ namespace PBSDumpWorkload
                                  new SqlParameter("@pintLoadsDumpControlID", loadsId),
                                  new SqlParameter("@pvchrBNTimeStamp", fileInfo.LastWriteTime));
 
-            ProcessFile(fileInfo);
+            ProcessFile(fileInfo, loadsId);
 
         }
 
-        private void ProcessFile(FileInfo fileInfo)
+        private void ProcessFile(FileInfo fileInfo, Int32 loadsId)
         {
             WriteToJobLog(JobLogMessageType.INFO, $"Reading {fileInfo.Name}");
+
+            List<Dictionary<string, object>> tables = new List<Dictionary<string, object>>();
+            bool pbsDumpFileVersion = false;
+            bool suppliedDumpFileVersion = false;
 
             if (fileInfo.Length > 0)
             {
@@ -140,9 +144,187 @@ namespace PBSDumpWorkload
                 {
                     List<string> segments = line.Split('|').ToList();
 
+                    Dictionary<string, object> table = new Dictionary<string, object>();
+
+                    if (segments[0] == "PBSDump1")
+                    {
+                        table.Add("LoadsTableID", 0);
+                        table.Add("GroupNumber", segments[1]);
+                        table.Add("FromDate", segments[2]);
+                        table.Add("TableName", segments[3]);
+                        table.Add("FileNameWithoutExtension", segments[4]);
+                        table.Add("UpdateTranNumberFileAfterSuccessfulPopulate", false);
+                        table.Add("UpdateTranDateAfterSuccessfulPopulate", segments[5]);
+                        table.Add("TableDumpStartDateTime", segments[6]);
+
+                        pbsDumpFileVersion = true;
+                        suppliedDumpFileVersion = false;
+                    }
+                    else if (segments[0] == "CircDump1")
+                    {
+                        table.Add("LoadsTableID", 0);
+                        table.Add("GroupNumber", segments[1]);
+                        table.Add("FromDate", segments[2]);
+                        table.Add("ArchiveEndingDate", segments[3]);
+                        table.Add("TableName", segments[4]);
+                        table.Add("FileNameWithoutExtension", segments[5]);
+                        table.Add("UpdateTranNumberFileAfterSuccessfulPopulate", segments[6]);
+                        table.Add("UpdateTranDateAfterSuccessfulPopulate", false);
+                        table.Add("TableDumpStartDateTime", segments[7]);
+
+                        pbsDumpFileVersion = false;
+                        suppliedDumpFileVersion = false;
+                    }
+                    else if (segments[0] == "SuppliesDump1" | segments[0] == "SuppliesDump")
+                    {
+                        table.Add("LoadsTableID", 0);
+                        table.Add("GroupNumber", segments[1]);
+                        table.Add("FromDate", segments[2]);
+                        table.Add("ArchiveEndingDate", "");
+                        table.Add("TableName", segments[3]);
+                        table.Add("FileNameWithoutExtension", segments[4]);
+                        table.Add("UpdateTranNumberFileAfterSuccessfulPopulate", false);
+                        table.Add("UpdateTranDateAfterSuccessfulPopulate", false);
+                        table.Add("TableDumpStartDateTime", segments[5]);
+
+                        pbsDumpFileVersion = false;
+                        suppliedDumpFileVersion = true;
+                    }
+                    else
+                    {
+                        WriteToJobLog(JobLogMessageType.WARNING, $"DumpControl File_Version {segments[0]} not defined");
+                        //todo: should we send an email? Is this ever a real case?
+                        return;
+                    }
+
+                   tables.Add(table);
+
+                    //delete touch file
+                    if (File.Exists(GetConfigurationKeyValue("TableTouchDirectory") + table["TableName"] + ".successful"))
+                        File.Delete(GetConfigurationKeyValue("TableTouchDirectory") + table["TableName"] + ".successful");
+
+                    //create group folder path if doesn't exist
+                    if (!Directory.Exists(GetConfigurationKeyValue("GroupTouchDirectory") + table["GroupNumber"]))
+                        Directory.CreateDirectory(GetConfigurationKeyValue("GroupTouchDirectory") + table["GroupNumber"]);
+
+                    //if the file already exists, delete it
+                    if (File.Exists(GetConfigurationKeyValue("GroupTouchDirectory") + table["GroupNumber"] + "\\" + table["TableName"] + ".successful"))
+                        File.Delete(GetConfigurationKeyValue("GroupTouchDirectory") + table["GroupNumber"] + "\\" + table["TableName"] + ".successful");
 
                 }
             }
+
+            if (tables.Count > 0)
+            {
+                //todo: this is for the circdump specific version
+                //07/12/20 PEB - Added support for CircDump as part of the Newscycle Cloud migration.
+                //Append the group number to this record so it's unique to the group of CircDump datasets
+                //If(mflgIsCircDump) Then
+                //gcnnSQL.Proc_Update_BN_Loads_DumpControl_OriginalFile mlngLoadsDumpControlID, gobjLoad.OriginalFileName & "_" & audfTables(0).intGroupNumber
+                //End If
+
+                //This sproc gets "populate immediately" flag for each group.  For CircDump this flag is 0 for all groups.
+                Dictionary<string, object> result = ExecuteSQL(DatabaseConnectionStringNames.CircDumpWork, "Proc_Select_BN_Groups",
+                                                                new SqlParameter("@pintGroupNumber", tables[0]["GroupNumber"])).FirstOrDefault();
+
+                if (result == null)
+                {
+                    WriteToJobLog(JobLogMessageType.ERROR, $"Group number ({tables[0]["GroupNumber"]} not found)");
+                    //todo: should we send an email? Is this ever a real case?
+                    return;
+                }
+
+                DateTime fromDate;
+                if (DateTime.TryParse(tables[0]["FromDate"].ToString(), out fromDate))
+                    WriteToJobLog(JobLogMessageType.INFO, $"For group number {tables[0]["GroupNumber"]} , dump's from date {tables[0]["FromDate"]}");
+                else
+                    WriteToJobLog(JobLogMessageType.INFO, $"For group number {tables[0]["GroupNumber"]} , all records selected");
+
+                bool populateImmediatelyAfterLoad = bool.Parse(result["populate_immediately_after_load_flag"].ToString());
+                bool atleastOneWorkToLoad = false;
+
+                if (populateImmediatelyAfterLoad)
+                {
+                    atleastOneWorkToLoad = true;
+
+                    if (pbsDumpFileVersion)
+                    {
+                        //todo: this sproc doesn't exist in the current database, so hopefully this condition is never hit
+                        //ExecuteNonQuery(DatabaseConnectionStringNames.CircDumpWork, "Proc_Update_BN_Loads_DumpControl_Group_Number_TranDate",
+                        //                                new SqlParameter("", ""),
+                        //                                new SqlParameter("", ""),
+                        //                                new SqlParameter("", ""),
+                        //                                new SqlParameter("", ""));
+
+                     }
+                    else
+                    {
+                        ExecuteNonQuery(DatabaseConnectionStringNames.CircDumpWork, "Proc_Update_BN_Loads_DumpControl_Group_Number",
+                                                        new SqlParameter("@pintLoadsDumpControlID", loadsId),
+                                                        new SqlParameter("@pintGroupNumber", tables[0]["GroupNumber"].ToString()));
+                    }
+                }
+                else
+                {
+                    //Update the GroupNumber in the current record in BN_Loads_DumpControl
+                    if (pbsDumpFileVersion)
+                    {
+                        //todo: this condition must not be hit, since the old version of the coode has an extra parameter and there's no matching sproc
+                        //ExecuteNonQuery(DatabaseConnectionStringNames.CircDumpWork, "Proc_Update_BN_Loads_DumpControl_Group_Number",
+                                //new SqlParameter("@pintLoadsDumpControlID", loadsId),
+                                //new SqlParameter("@pintGroupNumber", tables[0]["GroupNumber"]));
+                    } else
+                    {
+                        ExecuteNonQuery(DatabaseConnectionStringNames.CircDumpWork, "Proc_Update_BN_Loads_DumpControl_Group_Number",
+                                new SqlParameter("@pintLoadsDumpControlID", loadsId),
+                                new SqlParameter("@pintGroupNumber", tables[0]["GroupNumber"].ToString()));
+                    }
+
+                    //create the table records and return the loads_id for each
+                    foreach (Dictionary<string, object> table in tables)
+                    {
+                        if (table["FileNameWithoutExtension"].ToString() != "")
+                        {
+                            atleastOneWorkToLoad = true;
+
+                            if (pbsDumpFileVersion)
+                            {
+                              result =  ExecuteSQL(DatabaseConnectionStringNames.CircDumpWork, "Proc_Insert_BN_Loads_Tables",
+                                                    new SqlParameter("@pvchrTableName", table["TableName"]),
+                                                    new SqlParameter("@pbintLoadsDumpControlID", loadsId),
+                                                    new SqlParameter("@pvchrTableDumpStartDateTime", table["TableDumpStartDateTime"].ToString()),
+                                                    new SqlParameter("@pvchrFromDate", table["FromDate"].ToString())).FirstOrDefault();
+                            } 
+                            else
+                            {
+                               result = ExecuteSQL(DatabaseConnectionStringNames.CircDumpWork, "Proc_Insert_BN_Loads_Tables",
+                                                    new SqlParameter("@pvchrTableName", table["TableName"]),
+                                                    new SqlParameter("@pbintLoadsDumpControlID", loadsId),
+                                                    new SqlParameter("@pvchrTableDumpStartDateTime", table["TableDumpStartDateTime"].ToString()),
+                                                    new SqlParameter("@pvchrFromDate", table["FromDate"].ToString()),
+                                                    new SqlParameter("@pvchrArchiveEndingDate", table["ArchiveEndingDate"].ToString()),
+                                                    new SqlParameter("@pflgUpdateTranNumberControlFileAfterPopulate", table["UpdateTranNumberFileAfterSuccessfulPopulate"].ToString())).FirstOrDefault();
+                            }
+
+                            table["LoadsTableID"] = result["loads_table_id"].ToString();
+                        }
+                    }
+
+
+                    //todo: this must not be hit, there's no matching sproc with this name
+                    //if (pbsDumpFileVersion)
+                    //    ExecuteNonQuery(DatabaseConnectionStringNames.CircDumpWork, "Proc_Insert_BN_DumpControl_Post_Load", new SqlParameter("", loadsId));
+
+                    //Here is where the actual data import takes place, via a bulk insert.
+                    if (atleastOneWorkToLoad)
+                    {
+
+                    }
+
+
+                }
+            }
+
         }
     }
 }
