a8dab1532f78ee8069719ac04612d808d14b8a2b
Greg Sipes <gregory.sipes@gmail.com>
2020-11-16 15:47:04 -0500

    building feeds

--- Feeds/Job.cs
+++ Feeds/Job.cs
@@ -115,7 +115,7 @@ namespace Feeds
 
 
             //retrieve the rest of the feed specific fields
-            result = ExecuteSQL(DatabaseConnectionStringNames.Feeds, "Proc_Select_Feeds",
+            Dictionary<string, object> feed = ExecuteSQL(DatabaseConnectionStringNames.Feeds, "Proc_Select_Feeds",
                                                         new SqlParameter("@pvchrTitle", Version),
                                                         new SqlParameter("@pflgActiveOnly", 0),
                                                         new SqlParameter("@pvchrPassPhrase", ""),
@@ -180,6 +180,116 @@ namespace Feeds
                                             new SqlParameter("@pvchrNetworkUserName", System.Security.Principal.WindowsIdentity.GetCurrent().Name),
                                             new SqlParameter("@pvchrComputerName", System.Environment.MachineName.ToLower())).FirstOrDefault();
 
+            Int64 buildId = Convert.ToInt64(result["builds_id"].ToString());
+
+            //Invoke 'Proc_Select_Formats to get the correct format for this particular build.
+            //formats_id is the parameter that selects for the specfic build.
+            //(Example:  In the case of TSExport, formats_id = 4)
+
+            Dictionary<string, object> format = null;
+            List<Dictionary<string, object>> fields = null;
+
+            if (Convert.ToInt32(feed["formats_id"].ToString()) != 0)
+            {
+                //get format
+                WriteToJobLog(JobLogMessageType.INFO, "Determining feed file format.");
+                format = ExecuteSQL(DatabaseConnectionStringNames.Feeds, "Proc_Select_Formats", new SqlParameter("@pintFormatsID", Convert.ToInt32(feed["formats_id"].ToString()))).FirstOrDefault();
+
+                //get fields
+                WriteToJobLog(JobLogMessageType.INFO, "Determining fields associated with feed file format.");
+                fields = ExecuteSQL(DatabaseConnectionStringNames.Feeds, "Proc_Select_Fields", new SqlParameter("@pintFormatsID", Convert.ToInt32(feed["formats_id"].ToString()))).ToList();
+
+            }
+
+
+            //Aging summary (special case, only when this build's aging_summary_flag is set to 1)
+            if (Convert.ToBoolean(feed["aging_summary_flag"].ToString()))
+            {
+                WriteToJobLog(JobLogMessageType.INFO, "Assigning userserialno for Aging Summary.");
+
+                result = ExecuteSQL(DatabaseConnectionStringNames.Feeds, "Proc_Select_UserSerialNos").FirstOrDefault();
+
+                Int64 userSerialNumber = Convert.ToInt64(result["userserialno"].ToString());
+
+                WriteToJobLog(JobLogMessageType.INFO, "Retrieving dates for Aging Summary.");
+
+                result = ExecuteSQL(DatabaseConnectionStringNames.Feeds, "Proc_Select_tblSites",
+                                    new SqlParameter("@pvchrBWServerInstance", GetConfigurationKeyValue("RemoteServerName")),
+                                    new SqlParameter("@pvchrBWDatabase", GetConfigurationKeyValue("RemoteDatabaseName")),
+                                    new SqlParameter("@pvchrBWUserName", GetConfigurationKeyValue("RemoteUserName")),
+                                    new SqlParameter("@pvchrBWPassword", GetConfigurationKeyValue("RemotePassword"))).FirstOrDefault();
+
+                ExecuteNonQuery(DatabaseConnectionStringNames.Brainworks, "PrepareAsOfAgingSummarynew",
+                                    new SqlParameter("@asofagingdate", DateTime.Now.ToShortDateString()),
+                                    new SqlParameter("@current", Convert.ToDateTime(result["periodstartdate"].ToString()).ToShortDateString()),
+                                    new SqlParameter("@days30", Convert.ToDateTime(result["days30"].ToString()).ToShortDateString()),
+                                    new SqlParameter("@days60", Convert.ToDateTime(result["days60"].ToString()).ToShortDateString()),
+                                    new SqlParameter("@days90", Convert.ToDateTime(result["days90"].ToString()).ToShortDateString()),
+                                    new SqlParameter("@UserSerialno", userSerialNumber));
+
+                //Invoke the appropriate stored procedure (from the build record field "stored_proc" in table Feeds)
+                DetermineParameters(feedId, buildId, Convert.ToInt64(feed["pubid"].ToString()), userSerialNumber);
+
+            }
+        }
+
+        private List<SqlParameter> DetermineParameters(Int32 feedId, Int64 buildId, Int64 pubId, Int64 userSerialNumber)
+        {
+            //Different ad types have different fields/parameters associated with them.
+            //Here is where we select the appropriate parameters based on the FeedsID
+            //(which, of course, is passed in as a global parameter and not a calling parameter,
+            //  just to make things difficult to maintain).
+            List<SqlParameter> parameters = new List<SqlParameter>();
+
+            List<Dictionary<string, object>> results = ExecuteSQL(DatabaseConnectionStringNames.Feeds, "Proc_Select_Parameters", new SqlParameter("@pintFeedsID", feedId)).ToList();
+
+            foreach (Dictionary<string, object> result in results)
+            {
+                switch (result["parameter_name"].ToString())
+                {
+                    case "builds_id":
+                        parameters.Add(new SqlParameter("", buildId));
+                        break;
+                    case "bw_database":
+                        parameters.Add(new SqlParameter("", GetConfigurationKeyValue("RemoteDatabaseName")));
+                        break;
+                    case "bw_server_instance":
+                        parameters.Add(new SqlParameter("", GetConfigurationKeyValue("RemoteServerName")));
+                        break;
+                    case "ending_date":
+                        parameters.Add(new SqlParameter("", DateTime.Now.AddDays(Convert.ToDouble(result["days_to_add"].ToString())).AddDays(Convert.ToDouble(result["noninteractive_ending_date_days_after_starting_date"].ToString()))));
+                        break;
+                    case "false":
+                        parameters.Add(new SqlParameter("", false));
+                        break;
+                    case "password":
+                        parameters.Add(new SqlParameter("", GetConfigurationKeyValue("RemotePassword")));
+                        break;
+                    case "pbsdumpb_database":
+                        parameters.Add(new SqlParameter("", GetConfigurationKeyValue("PBSDumpBDatabaseName")));
+                        break;
+                    case "pbsdumpb_server_instance":
+                        parameters.Add(new SqlParameter("", GetConfigurationKeyValue("PBSDumpBServerName")));
+                        break;
+                    case "pubid":
+                        parameters.Add(new SqlParameter("", pubId));
+                        break;
+                    case "starting_date":
+                        parameters.Add(new SqlParameter("", DateTime.Now.AddDays(Convert.ToDouble(result["days_to_add"].ToString()))));
+                        break;
+                    case "true":
+                        parameters.Add(new SqlParameter("", true));
+                        break;
+                    case "user_name":
+                        parameters.Add(new SqlParameter("", result["user_name"].ToString()));
+                        break;
+                    case "userserialno":
+                        parameters.Add(new SqlParameter("", userSerialNumber));
+                        break;
+                }
+            }
+
+            return parameters;
 
         }
     }
